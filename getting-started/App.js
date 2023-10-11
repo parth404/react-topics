6b2e8ddca64ecb12eff46c49cf377dece614fe10
Parth Bhattacharya <parthget@gmail.com>
2023-10-11 22:17:53 +0530

    creating nested structures

--- getting-started/App.js
+++ getting-started/App.js
@@ -1,8 +1,21 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", style: { color: "red" } },
-  "Hello World from React"
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", style: { color: "red" } },
+//   "Hello World from React"
+// );
+
+// creating nested elements
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "1" }, "I am H1 tag"),
+    React.createElement("h2", { key: "2" }, "I am H2 tag"),
+  ])
 );
 
+console.log(parent);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
